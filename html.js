7a3bf8994486b6e03348b2cad741965c54ab3416
pedro.sorto <pedro.sorto@digitalglobe.com>
2017-08-16 13:32:42 -0400

    updating dir files

--- html.js
+++ html.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 256);
+/******/ 	return __webpack_require__(__webpack_require__.s = 248);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 256:
+/***/ 248:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__.p + "index.html";
